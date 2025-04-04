bec4adb34e11c26ef25f9b763c7e12f88a31f0a2
nielshn <niel.shn08@gmail.com>
2025-04-04 13:01:24 +0700

    pull request

--- resources/views/MasterData/Satuan/index.blade.php
+++ resources/views/MasterData/Satuan/index.blade.php
@@ -1,13 +1,11 @@
 <!-- Main navbar -->
 @push('css')
-<script src="{{asset('template/assets/js/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('template/assets/js/vendor/tables/datatables/datatables.min.js')}}"></script>
-<script src="{{asset('template/assets/js/vendor/tables/datatables/extensions/pdfmake/pdfmake.min.js')}}"></script>
-<script src="{{asset('template/assets/js/vendor/tables/datatables/extensions/pdfmake/vfs_fonts.min.js')}}"></script>
-<script src="{{asset('template/assets/js/vendor/tables/datatables/extensions/buttons.min.js')}}"></script>
-<script src="{{asset('template/assets/demo/pages/datatables_extension_buttons_html5.js')}}"></script>
-
-
+    <script src="{{ asset('template/assets/js/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('template/assets/js/vendor/tables/datatables/datatables.min.js') }}"></script>
+    <script src="{{ asset('template/assets/js/vendor/tables/datatables/extensions/pdfmake/pdfmake.min.js') }}"></script>
+    <script src="{{ asset('template/assets/js/vendor/tables/datatables/extensions/pdfmake/vfs_fonts.min.js') }}"></script>
+    <script src="{{ asset('template/assets/js/vendor/tables/datatables/extensions/buttons.min.js') }}"></script>
+    <script src="{{ asset('template/assets/demo/pages/datatables_extension_buttons_html5.js') }}"></script>
 @endpush
 @include('layouts.navbar')
 
@@ -28,115 +26,120 @@
         <div class="content-inner">
 
             <!-- Page header -->
-           @include('layouts.page_header')
+            @include('layouts.page_header')
             <!-- /page header -->
 
             <!-- Centered modal -->
-	<div id="modal_centered" class="modal fade" tabindex="-1">
-		<div class="modal-dialog modal-dialog-centered modal-xl">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title">Tambah Data</h5>
-					<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-				</div>
-
-				<div class="modal-body">
-					<h6 class="fw-semibold">Text in a modal</h6>
-					<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-
-					<hr>
-
-					<h6 class="fw-semibold">Another paragraph</h6>
-					<p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-					<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
-				</div>
-
-				<div class="modal-footer">
-					<button type="button" class="btn btn-link" data-bs-dismiss="modal">Close</button>
-					<button type="button" class="btn btn-primary">Save changes</button>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- /centered modal -->
-
-
-
-
-<!-- Content area -->
-<div class="content">
-    <button type="button" class="btn btn-primary btn-labeled btn-labeled-start mb-2" data-bs-toggle="modal" data-bs-target="#modal_centered">
-        <span class="btn-labeled-icon bg-black bg-opacity-20">
-            <i class="icon-database-add"></i>
-        </span>
-        Tambah
-    </button>
-    <button type="button" id="scan-btn" class="btn btn-primary btn-labeled btn-labeled-start mb-2">
-        <span class="btn-labeled-icon bg-black bg-opacity-20">
-            <i class="ph-scan ph-2x rounded"></i>
-        </span>
-        Scan
-    </button>
-               <!-- Container Scanner -->
-               <div id="scanner-container" style="display: none;">
-                <button id="close-btn">✖</button>
-                <video id="preview"></video>
-                <input type="text" id="qrcode-result" class="form-control mt-2" readonly>
-            </div>
-
-
-    <!-- Basic initialization -->
-    <div class="card">
-        <div class="card-header">
-            <h5 class="mb-0">Data Barang</h5>
-        </div>
-
-        <table class="table datatable-button-html5-basic">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>name</th>
-                    <th>slug</th>
-                    <th>deskripsi</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($data['data'] as $key => $satuan) <tr>
-                    <td>{{ $key + 1 }}</td>
-                    <td>{{$satuan['name']}}</td>
-                    <td>{{ $satuan['description'] ?? '-' }}</td>
-                    <td><span class="badge bg-success bg-opacity-10 text-success">Active</span></td>
-                    <td></td>
-                    <td>
-                        <div class="d-inline-flex">
-                            <a href="#" class="text-primary">
-                                <i class="ph-pen"></i>
-                            </a>
-                            <a href="#" class="text-danger mx-2">
-                                <i class="ph-trash"></i>
-                            </a>
-                            <a href="#" class="text-teal">
-                                <i class="ph-gear"></i>
-                            </a>
+            <div id="modal_centered" class="modal fade" tabindex="-1">
+                <div class="modal-dialog modal-dialog-centered modal-xl">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Tambah Data</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                         </div>
-                    </td>
-                </tr>
 
-                @endforeach
+                        <div class="modal-body">
+                            <h6 class="fw-semibold">Text in a modal</h6>
+                            <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem.
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                                lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
 
-            </tbody>
-        </table>
-    </div>
-    <!-- /basic initialization -->
+                            <hr>
+
+                            <h6 class="fw-semibold">Another paragraph</h6>
+                            <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
+                                in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis
+                                lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-link" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /centered modal -->
+
+
+
+
+            <!-- Content area -->
+            <div class="content">
+                <button type="button" class="btn btn-primary btn-labeled btn-labeled-start mb-2" data-bs-toggle="modal"
+                    data-bs-target="#modal_centered">
+                    <span class="btn-labeled-icon bg-black bg-opacity-20">
+                        <i class="icon-database-add"></i>
+                    </span>
+                    Tambah
+                </button>
+                <button type="button" id="scan-btn" class="btn btn-primary btn-labeled btn-labeled-start mb-2">
+                    <span class="btn-labeled-icon bg-black bg-opacity-20">
+                        <i class="ph-scan ph-2x rounded"></i>
+                    </span>
+                    Scan
+                </button>
+                <!-- Container Scanner -->
+                <div id="scanner-container" style="display: none;">
+                    <button id="close-btn">✖</button>
+                    <video id="preview"></video>
+                    <input type="text" id="qrcode-result" class="form-control mt-2" readonly>
+                </div>
+
+
+                <!-- Basic initialization -->
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="mb-0">Data Barang</h5>
+                    </div>
+
+                    <table class="table datatable-button-html5-basic">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>name</th>
+                                <th>slug</th>
+                                <th>deskripsi</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data['data'] as $key => $satuan)
+                                <tr>
+                                    <td>{{ $key + 1 }}</td>
+                                    <td>{{ $satuan['name'] }}</td>
+                                    <td>{{ $satuan['description'] ?? '-' }}</td>
+                                    <td><span class="badge bg-success bg-opacity-10 text-success">Active</span></td>
+                                    <td></td>
+                                    <td>
+                                        <div class="d-inline-flex">
+                                            <a href="#" class="text-primary">
+                                                <i class="ph-pen"></i>
+                                            </a>
+                                            <a href="#" class="text-danger mx-2">
+                                                <i class="ph-trash"></i>
+                                            </a>
+                                            <a href="#" class="text-teal">
+                                                <i class="ph-gear"></i>
+                                            </a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </tbody>
+                    </table>
+                </div>
+                <!-- /basic initialization -->
 
 
 
 
 
 
-</div>
-<!-- /content area -->
+            </div>
+            <!-- /content area -->
 
 
             <!-- Content area -->
@@ -147,27 +150,27 @@
             @include('layouts.footer')
 
 
-        <!-- /inner content -->
+            <!-- /inner content -->
+
+        </div>
+        <!-- /main content -->
 
     </div>
-    <!-- /main content -->
 
-</div>
-
-<!-- /page content -->
+    <!-- /page content -->
 
 
-<!-- Notifications -->
-@include('layouts.components.notifications')
+    <!-- Notifications -->
+    @include('layouts.components.notifications')
 
-<!-- /notifications -->
+    <!-- /notifications -->
 
 
-<!-- Demo config -->
-@include('layouts.components.demo_config')
+    <!-- Demo config -->
+    @include('layouts.components.demo_config')
 
-<!-- /demo config -->
+    <!-- /demo config -->
 
-</body>
+    </body>
 
-</html>
+    </html>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,8 +23,8 @@
     <script src="{{ asset('template/assets/js/vendor/visualization/d3/d3_tooltip.js') }}"></script>
 
     <script src="{{ asset('assets/js/app.js') }}"></script>
-    <script src="{{asset('template/assets/js/vendor/notifications/noty.min.js')}}"></script>
-	<script src="{{asset('template/assets/demo/pages/extra_noty.js')}}"></script>
+    <script src="{{ asset('template/assets/js/vendor/notifications/noty.min.js') }}"></script>
+    <script src="{{ asset('template/assets/demo/pages/extra_noty.js') }}"></script>
     <script src="{{ asset('template/assets/demo/pages/dashboard.js') }}"></script>
     <script src="{{ asset('template/assets/demo/charts/pages/dashboard/streamgraph.js') }}"></script>
     <script src="{{ asset('template/assets/demo/charts/pages/dashboard/sparklines.js') }}"></script>
@@ -47,7 +47,8 @@
             <div class="navbar-brand">
                 <a href="index.html" class="d-inline-flex align-items-center">
                     <img src="../../../assets/images/logo_icon.svg" alt="">
-                    <img src="../../../assets/images/logo_text_light.svg" class="d-none d-sm-inline-block h-16px ms-3" alt="">
+                    <img src="../../../assets/images/logo_text_light.svg" class="d-none d-sm-inline-block h-16px ms-3"
+                        alt="">
                 </a>
             </div>
         </div>
@@ -65,57 +66,62 @@
 
                 <!-- Content area -->
                 <div class="content d-flex justify-content-center align-items-center">
-                   <!-- Menampilkan pesan error dari API -->
-@if(session('error_message'))
-<div class="alert alert-danger text-center mb-3">
-    {{ session('error_message') }}
-</div>
-@endif
-<form class="login-form" action="{{ route('auth.login') }}" method="POST">
-    @csrf
-    <div class="card mb-0">
-        <div class="card-body">
-            <div class="text-center mb-3">
-                <img src="{{ asset('template/assets/images/logo_icon.svg') }}" class="h-48px" alt="">
-                <h5 class="mb-0">Login to your account</h5>
-                <span class="d-block text-muted">Enter your credentials below</span>
-            </div>
+                    <!-- Menampilkan pesan error dari API -->
+                    @if (session('error_message'))
+                        <div class="alert alert-danger text-center mb-3">
+                            {{ session('error_message') }}
+                        </div>
+                    @endif
+                    @if (session('login_success'))
+                        <div class="alert alert-success">{{ session('login_success') }}</div>
+                    @endif
+                    <form class="login-form" action="{{ route('auth.login') }}" method="POST">
+                        @csrf
+                        <div class="card mb-0">
+                            <div class="card-body">
+                                <div class="text-center mb-3">
+                                    <img src="{{ asset('template/assets/images/logo_icon.svg') }}" class="h-48px"
+                                        alt="">
+                                    <h5 class="mb-0">Login to your account</h5>
+                                    <span class="d-block text-muted">Enter your credentials below</span>
+                                </div>
 
-            <!-- Tampilkan pesan error -->
-            @if($errors->has('login_error'))
-            <div class="alert alert-danger">
-                {{ $errors->first('login_error') }}
-            </div>
-        @endif
+                                <!-- Tampilkan pesan error -->
+                                @if ($errors->has('login_error'))
+                                    <div class="alert alert-danger">
+                                        {{ $errors->first('login_error') }}
+                                    </div>
+                                @endif
 
 
+                                <div class="mb-3">
+                                    <label class="form-label">Email</label>
+                                    <div class="form-control-feedback form-control-feedback-start">
+                                        <input type="email" name="email" class="form-control"
+                                            placeholder="Masukan email.." required>
+                                        <div class="form-control-feedback-icon">
+                                            <i class="ph-user-circle text-muted"></i>
+                                        </div>
+                                    </div>
+                                </div>
 
-            <div class="mb-3">
-                <label class="form-label">Email</label>
-                <div class="form-control-feedback form-control-feedback-start">
-                    <input type="email" name="email" class="form-control" placeholder="Masukan email.." required>
-                    <div class="form-control-feedback-icon">
-                        <i class="ph-user-circle text-muted"></i>
-                    </div>
-                </div>
-            </div>
+                                <div class="mb-3">
+                                    <label class="form-label">Password</label>
+                                    <div class="form-control-feedback form-control-feedback-start">
+                                        <input type="password" name="password" class="form-control"
+                                            placeholder="•••••••••••" required>
+                                        <div class="form-control-feedback-icon">
+                                            <i class="ph-lock text-muted"></i>
+                                        </div>
+                                    </div>
+                                </div>
 
-            <div class="mb-3">
-                <label class="form-label">Password</label>
-                <div class="form-control-feedback form-control-feedback-start">
-                    <input type="password" name="password" class="form-control" placeholder="•••••••••••" required>
-                    <div class="form-control-feedback-icon">
-                        <i class="ph-lock text-muted"></i>
-                    </div>
-                </div>
-            </div>
-
-            <div class="mb-3">
-                <button type="submit" class="btn btn-primary w-100">Login</button>
-            </div>
-        </div>
-    </div>
-</form>
+                                <div class="mb-3">
+                                    <button type="submit" class="btn btn-primary w-100">Login</button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
 
 
 
